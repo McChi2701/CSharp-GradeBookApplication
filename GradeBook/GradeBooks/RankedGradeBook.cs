7fe8b29049490a3aa67483be93c85f679ea2fa0d
James <chijames@hotmail.com>
2018-10-11 09:38:56 +1100

    Update GradeBook/GradeBooks/RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
-             if (Student.Count < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
